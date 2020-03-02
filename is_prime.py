74a4224e044e609e9eee873f13448c6b9a341369
Alexa Kelly <59925735+kellyav@users.noreply.github.com>
2020-03-02 13:22:53 -0500

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,8 @@
 # this function to check if the given number is prime or not. retuns True or False accordingly. 
 def is_prime(n):
-    if n % 2 == 0:
+    if n==2:
+        return True
+    elif n % 2 == 0:
         return False
     else:      
         for i in range(3, int(n**0.5+1),2): 
