371b711229b84955c7c6bbafe142882e6a863d9f
Alexa Kelly <59925735+kellyav@users.noreply.github.com>
2020-04-14 15:01:53 -0400

    Update factorial.py

--- supporting functions/factorial.py
+++ supporting functions/factorial.py
@@ -1,5 +1,7 @@
 def factorial(x):
-    if x == 0:
+    if x<0: 
+        print("Factorial doesn't exist for negative numbers!")
+    elif x == 0:
         return 1
     else:
         return x*factorial(x-1)
